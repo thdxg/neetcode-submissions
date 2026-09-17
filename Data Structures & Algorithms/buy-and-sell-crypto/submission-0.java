81747d427e1b07a9d27f7cd120db2d630a8d9f00
Ethan (Taehoon) Lee <48551278+thdxg@users.noreply.github.com>
2026-09-16 20:34:15 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > max) max = profit;
+            }
+        }
+        return max;
+    }
+}
