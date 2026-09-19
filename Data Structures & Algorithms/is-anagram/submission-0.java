ff6b087ce66deafdc2fff503e5068ea75c6fc72b
Ethan (Taehoon) Lee <48551278+thdxg@users.noreply.github.com>
2026-09-19 12:11:54 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            if (counts[c - 'a'] == 0) return false;
+            counts[c - 'a']--;
+        }
+        return true;
+    }
+}
